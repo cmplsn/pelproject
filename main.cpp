32fb29c695a3859cfaffe56abb9a7b22fed3500e
alessandrocampagnaro <cmplsn97@gmail.com>
2022-05-07 12:41:06 +0200

    .

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 #include "player.hpp"
 
 int main() {
-    Player a (1);
-    a.init_board("../boards/es.txt");
-    a.load_board("../boards/es.txt");
-    a.store_board("../boards/es_1.txt",0);
-    Player b(2);
 
     return 0;
 }
